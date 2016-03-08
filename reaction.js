eebbc95a2522a4b8baa533d990562fff7368d041
Sebastián Cruz <cruzjesussebastian@gmail.com>
2016-03-08 07:34:28 -0300

    Implementing Google Chrome extension stuff
    * Add code improvements
    * Add required extension files
    * Updates the reaction icon

--- reaction.js
+++ reaction.js
@@ -1,12 +1,19 @@
-
 setInterval(function(){
-	//Reescribe la popup de reactions
-	var el = document.querySelectorAll('div._4sm1')
-	for(e in el){if (el[e].innerText === "Angry") el[e].innerText = "Vamo a Calmarno"; }
-	//Reescribe las reactions ya hechas
-	var el = document.querySelectorAll('.uiContextualLayerParent._khz span')
-	for(e in el){if (el[e].innerText === "Angry") el[e].innerText = "Vamo a Calmarno"; }
+  // Reescribe la popup de reactions
+  var el = document.querySelectorAll('div._4sm1');
 
-},200);
+  for (e in el) {
+    if (el[e].innerText === "Angry" || el[e].innerText === "Me enoja") {
+      el[e].innerText = "Vamo a Calmarno";
+    }
+  }
 
+  // Reescribe las reactions ya hechas
+  var el = document.querySelectorAll('.uiContextualLayerParent._khz span');
 
+  for (e in el) {
+    if (el[e].innerText === "Angry" || el[e].innerText === "Me enoja") {
+      el[e].innerText = "Vamo a Calmarno";
+    }
+  }
+}, 200);
